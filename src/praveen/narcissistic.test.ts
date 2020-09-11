ad39e818daa8ec18993adf7a9b8522aa140779d1
Praveen Sisodiya <praveen@unifytech.com>
2020-09-11 17:35:57 +0530

    Fixed some PR comments

--- src/praveen/narcissistic.test.ts
+++ src/praveen/narcissistic.test.ts
@@ -3,47 +3,55 @@ import {
   checkIfNarcissisticNumber
 } from './narcissistic';
 
-describe('testing checkIfNarcissisticNumber function', function () {
-  it('testing if 153 is NarcissisticNumber ', function () {
+describe('testing checkIfNarcissisticNumber function', () => {
+  it('testing if 153 is NarcissisticNumber', () => {
     const result = checkIfNarcissisticNumber(153);
     expect(result).toBe(true);
   });
-  it('testing if 1634 is NarcissisticNumber ', function () {
+
+  it('testing if 1634 is NarcissisticNumber', () => {
     const result = checkIfNarcissisticNumber(1634);
     expect(result).toBe(true);
   });
-  it('testing if 150 is not NarcissisticNumber ', function () {
+
+  it('testing if 150 is not NarcissisticNumber', () => {
     const result = checkIfNarcissisticNumber(150);
     expect(result).toBe(false);
   });
-  it('testing if 10 is not NarcissisticNumber ', function () {
+
+  it('testing if 10 is not NarcissisticNumber', () => {
     const result = checkIfNarcissisticNumber(10);
     expect(result).toBe(false);
   });
-  it('testing if 0 is not NarcissisticNumber ', function () {
+
+  it('testing if 0 is not NarcissisticNumber', () => {
     const result = checkIfNarcissisticNumber(0);
     expect(result).toBe(false);
   });
-  it('testing if any negative number for eg. -5 is not NarcissisticNumber ', function () {
+
+  it('testing if any negative number for eg. -5 is not NarcissisticNumber', () => {
     const result = checkIfNarcissisticNumber(-5);
     expect(result).toBe(false);
   });
 });
 
-describe('testing getNarcissisticNumbersWithinRange function', function () {
-  it('testing if there is not narcissistic numbers between 10 and 20', function () {
+describe('testing getNarcissisticNumbersWithinRange function', () => {
+  it('testing if there is not narcissistic numbers between 10 and 20', () => {
     const result = getNarcissisticNumbersWithinRange(10, 20);
     expect(result).toEqual([]);
   });
-  it('testing if there is not narcissistic numbers between -10 and -20', function () {
+
+  it('testing if there is not narcissistic numbers between -10 and -20', () => {
     const result = getNarcissisticNumbersWithinRange(-10, -20);
     expect(result).toEqual([]);
   });
-  it('testing if there is not narcissistic numbers between 200 and 150', function () {
+
+  it('testing if there is not narcissistic numbers between 200 and 150', () => {
     const result = getNarcissisticNumbersWithinRange(200, 150);
     expect(result).toEqual([]);
   });
-  it('testing if there are 7 narcissistic numbers [153,  370,  371, 407, 1634, 8208, 9474] between 10 and 10000', function () {
+
+  it('testing if there are 7 narcissistic numbers [153,  370,  371, 407, 1634, 8208, 9474] between 10 and 10000', () => {
     const result = getNarcissisticNumbersWithinRange(10, 10000);
     const desiredResult = [153, 370, 371, 407, 1634, 8208, 9474];
     expect(result).toEqual(desiredResult);
--- src/praveen/narcissistic.ts
+++ src/praveen/narcissistic.ts
@@ -1,13 +1,11 @@
 /**
  * Function to find that the given number is Narcissistic Number or not
- * @date 2020-09-09
  * @param {number} num number to check if it is Narcissistic Number
  * @return {boolean} true or false
  */
 function checkIfNarcissisticNumber(num: number): boolean {
-  let isNarcissisticNumber = false;
   if (num <= 0) {
-    return isNarcissisticNumber;
+    return false;
   }
   const numberStr = num.toString();
   const numberLength = numberStr.length;
@@ -16,15 +14,11 @@ function checkIfNarcissisticNumber(num: number): boolean {
     const digit = parseInt(numberStr.substr(i, 1));
     sum += Math.pow(digit, numberLength);
   }
-  if (sum === num) {
-    isNarcissisticNumber = true;
-  }
-  return isNarcissisticNumber;
+  return sum === num;
 }
 
 /**
  * Function to find all the Narcissistic Numbers in the given range
- * @date 2020-09-09
  * @param {number} startNum starting number in the range
  * @param {number} endNum ending number in the range
  * @return {number[]} A array of Narcissistic Numbers
