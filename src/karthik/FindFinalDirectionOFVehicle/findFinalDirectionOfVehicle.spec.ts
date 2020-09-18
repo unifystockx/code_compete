d16e5cb2521fc69727d1e570f1357b0a93831e8d
Karthik Dheeraj <karthik@unifytech.com>
2020-09-18 12:44:50 +0530

    Optimised and reduced code, updated test cases with clear explanations, added error code and  added validity for initial direction

--- src/karthik/FindFinalDirectionOFVehicle/findFinalDirectionOfVehicle.spec.ts
+++ src/karthik/FindFinalDirectionOFVehicle/findFinalDirectionOfVehicle.spec.ts
@@ -1,77 +1,122 @@
 import { inputData } from './data';
 import {
   findFinalDirectionOfVehicle,
-  validMoves
+  validMoves,
+  validDirection
 } from './findFinalDirectionOfVehicle';
 
 describe('findFinalDirectionOfVehicle function', () => {
-  test('validity 1 for input data', () => {
+  test('Shall return the final direction as SOUTH after using inputData from initial position NORTH', () => {
     expect(findFinalDirectionOfVehicle(inputData, 'NORTH')).toEqual('SOUTH');
   });
 
-  test('validity 2 for expected directions', () => {
+  test('Shall return the final direction as NORTH after a move of STRAIGHT from initial position NORTH', () => {
     expect(findFinalDirectionOfVehicle(['STRAIGHT'], 'NORTH')).toEqual('NORTH');
   });
 
-  test('validity 3 for expected directions', () => {
+  test('Shall return the final direction as EAST after a move of RIGHT from initial position NORTH', () => {
     expect(findFinalDirectionOfVehicle(['RIGHT'], 'NORTH')).toEqual('EAST');
   });
 
-  test('validity 4 for expected directions', () => {
+  test('Shall return the final direction as SOUTH after a move of BACK from initial position NORTH', () => {
     expect(findFinalDirectionOfVehicle(['BACK'], 'NORTH')).toEqual('SOUTH');
   });
 
-  test('validity 5 for expected directions', () => {
+  test('Shall return the final direction as WEST after a move of LEFT from initial position NORTH', () => {
     expect(findFinalDirectionOfVehicle(['LEFT'], 'NORTH')).toEqual('WEST');
   });
 
-  test('validity 6 for turnaround move 1', () => {
+  test('Shall return the final direction as initial position NORTH with turnaround move', () => {
     expect(
       findFinalDirectionOfVehicle(['RIGHT', 'RIGHT', 'RIGHT', 'RIGHT'], 'NORTH')
     ).toEqual('NORTH');
   });
 
-  test('validity 7 for turnaround move 2', () => {
+  test('Shall return the final direction as initial position NORTH with turnaround move', () => {
     expect(findFinalDirectionOfVehicle(['BACK', 'BACK'], 'NORTH')).toEqual(
       'NORTH'
     );
   });
 
-  test('validity 8 for Empty moves', () => {
+  test('Shall return the final direction as initial position NORTH with an empty move', () => {
     expect(findFinalDirectionOfVehicle([], 'NORTH')).toEqual('NORTH');
   });
 
-  test('validity 9 for Invalid moves', () => {
-    expect(findFinalDirectionOfVehicle(['S'], 'NORTH')).toEqual(
+  test('Shall throw an error INVALID MOVES upon an invalid move', () => {
+    expect(() => findFinalDirectionOfVehicle(['S'], 'NORTH')).toThrow();
+    expect(() => findFinalDirectionOfVehicle(['S'], 'NORTH')).toThrowError(
       'INVALID MOVES'
     );
   });
 
-  test('validity 10 for Invalid moves', () => {
-    expect(
-      findFinalDirectionOfVehicle([...inputData, 'S', 'LEFT', 'BACK'], 'NORTH')
-    ).toEqual('INVALID MOVES');
+  test('Shall throw an error INVALID DIRECTION upon an invalid initial direction TOP', () => {
+    expect(() => findFinalDirectionOfVehicle(inputData, 'TOP')).toThrow();
+    expect(() => findFinalDirectionOfVehicle(inputData, 'TOP')).toThrowError(
+      'INVALID DIRECTION'
+    );
+  });
+
+  test('Shall throw an error INVALID DIRECTION upon an invalid initial direction DOWN', () => {
+    expect(() => findFinalDirectionOfVehicle(inputData, 'DOWN')).toThrow();
+    expect(() => findFinalDirectionOfVehicle(inputData, 'DOWN')).toThrowError(
+      'INVALID DIRECTION'
+    );
+  });
+
+  test('Shall throw an error INVALID MOVES upon (invalid moves & invalid initial direction TOP), validating the first parameter and throwing error', () => {
+    expect(() =>
+      findFinalDirectionOfVehicle([...inputData, 'S'], 'DOWN')
+    ).toThrow();
+    expect(() =>
+      findFinalDirectionOfVehicle([...inputData, 'S'], 'DOWN')
+    ).toThrowError('INVALID MOVES');
   });
 });
 
 describe('validMoves function ', () => {
-  test('valid data', () => {
+  test('Shall return true on valid moves', () => {
     expect(validMoves(inputData)).toEqual(true);
   });
 
-  test('valid data', () => {
+  test('Shall return true on valid moves', () => {
     expect(validMoves(['LEFT', 'RIGHT', 'STRAIGHT', 'BACK'])).toEqual(true);
   });
 
-  test('invalid data', () => {
+  test('Shall return false on invalid moves', () => {
     expect(validMoves([...inputData, 'STRAIGHT', 'RIG'])).toEqual(false);
   });
 
-  test('invalid data', () => {
+  test('Shall return false on invalid moves', () => {
     expect(validMoves([...inputData, '', 'RIGHT'])).toEqual(false);
   });
 
-  test('invalid data', () => {
+  test('Shall return false on invalid moves', () => {
     expect(validMoves(['STRAIGHT', 'RIG'])).toEqual(false);
+  });
+});
+
+describe('validDirection function ', () => {
+  test('Shall return true on valid direction NORTH', () => {
+    expect(validDirection('NORTH')).toEqual(true);
+  });
+
+  test('Shall return true on valid direction EAST', () => {
+    expect(validDirection('EAST')).toEqual(true);
+  });
+
+  test('Shall return true on valid direction SOUTH', () => {
+    expect(validDirection('SOUTH')).toEqual(true);
+  });
+
+  test('Shall return true on valid direction WEST', () => {
+    expect(validDirection('WEST')).toEqual(true);
+  });
+
+  test('Shall return false on invalid direction TOP', () => {
+    expect(validDirection('TOP')).toEqual(false);
+  });
+
+  test('Shall return false on invalid direction DOWN', () => {
+    expect(validDirection('DOWN')).toEqual(false);
   });
 });
--- src/karthik/FindFinalDirectionOFVehicle/findFinalDirectionOfVehicle.ts
+++ src/karthik/FindFinalDirectionOFVehicle/findFinalDirectionOfVehicle.ts
@@ -10,27 +10,21 @@ const all_moves = { STRAIGHT: 0, RIGHT: 1, BACK: 2, LEFT: 3 };
  *
  * @example
  *
- *     findFinalDirectionOfVehicle(['LEFT,BACK'],'NORTH')
+ *     findFinalDirectionOfVehicle(['LEFT','BACK'],'NORTH')
  */
 const findFinalDirectionOfVehicle = (
   moves: Array<string>,
   initialDirection: string
 ): string => {
+  if (!validMoves(moves)) throw new Error('INVALID MOVES');
+  if (!validDirection(initialDirection)) throw new Error('INVALID DIRECTION');
   if (!moves || moves.length < 1) return initialDirection;
-  if (!validMoves(moves)) return 'INVALID MOVES';
+
   let direction = initialDirection;
   let finalDirection = all_directions[direction];
 
   moves.forEach((move) => {
-    if (move === 'STRAIGHT') {
-      finalDirection = (all_directions[direction] + all_moves['STRAIGHT']) % 4;
-    } else if (move === 'RIGHT') {
-      finalDirection = (all_directions[direction] + all_moves['RIGHT']) % 4;
-    } else if (move === 'BACK') {
-      finalDirection = (all_directions[direction] + all_moves['BACK']) % 4;
-    } else if (move === 'LEFT') {
-      finalDirection = (all_directions[direction] + all_moves['LEFT']) % 4;
-    }
+    finalDirection = (all_directions[direction] + all_moves[move]) % 4;
 
     direction = Object.keys(all_directions).find(
       (key) => all_directions[key] === finalDirection
@@ -51,17 +45,22 @@ const findFinalDirectionOfVehicle = (
  *     validMoves(['STRAIGHT', 'RIGHT', 'LEFT'])
  */
 const validMoves = (moves: Array<string>): boolean => {
-  let validMove: boolean;
-  let res: boolean = true;
-  moves.forEach((move) => {
-    validMove = false;
-    for (const am in all_moves) {
-      validMove ||= String(am) === String(move);
-    }
-    res &&= validMove;
-  });
-
-  return res;
+  return moves.every((move) => all_moves[move] !== undefined);
 };
 
-export { findFinalDirectionOfVehicle, validMoves };
+/**
+ * Function to check if given valid direction as initial direction
+ *
+ * @param {string} direction - String containing initial direction
+ * @return {boolean} - true or false
+ *
+ * @example
+ *
+ *     validDirection('NORTH')
+ */
+const validDirection = (direction: string): boolean => {
+  return all_directions[direction] !== undefined;
+  // return [direction].every((dir) => all_directions[dir] !== undefined);
+};
+
+export { findFinalDirectionOfVehicle, validMoves, validDirection };
